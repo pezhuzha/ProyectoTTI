e4135dfc7fae081b1882c592bc9a46b9a7c1f7f1
Pedro <pezhuzha@unirioja.es>
2025-05-11 14:23:54 +0200

    add CMake para que vaya más rápido

---
+++ build/CMakeFiles/test.out.dir/timediff.cpp.o.d
@@ -0,0 +1,20 @@
+CMakeFiles/test.out.dir/timediff.cpp.o: \
+ /home/fghfgh/Escritorio/ProyectoTTI/src/timediff.cpp \
+ /usr/include/stdc-predef.h \
+ /home/fghfgh/Escritorio/ProyectoTTI/src/../include/timediff.h \
+ /usr/include/c++/13/tuple /usr/include/c++/13/bits/stl_pair.h \
+ /usr/include/c++/13/type_traits \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/cpu_defines.h \
+ /usr/include/c++/13/pstl/pstl_config.h /usr/include/c++/13/bits/move.h \
+ /usr/include/c++/13/bits/utility.h \
+ /usr/include/c++/13/bits/uses_allocator.h \
+ /usr/include/c++/13/bits/invoke.h
